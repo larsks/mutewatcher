75b8d002e4d06787fff7bbd2251109fdf3432683
Lars Kellogg-Stedman <lars@oddbit.com>
2021-09-30 08:28:19 -0400

    Successfully controls LED in response to mute changes
    With this commit, mutewatcher successfully controls the mute LEDs on
    my nanokontrol2 in response to changes in the mute state.

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/larsks/mutewatcher
 
 go 1.16
 
-require github.com/lawl/pulseaudio v0.0.0-20210928141934-ed754c0c6618
+require (
+	github.com/larsks/go-decouple v0.0.3
+	github.com/lawl/pulseaudio v0.0.0-20210928141934-ed754c0c6618
+	github.com/rs/zerolog v1.25.0
+	gitlab.com/gomidi/midi v1.23.7
+	gitlab.com/gomidi/rtmididrv v0.14.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/larsks/go-decouple"
 	"github.com/lawl/pulseaudio"
+	"github.com/rs/zerolog/log"
+	"gitlab.com/gomidi/midi"
+	"gitlab.com/gomidi/midi/writer"
+	driver "gitlab.com/gomidi/rtmididrv"
 )
 
 func must(err error) {
@@ -34,7 +37,37 @@ func IsSinkMuted(client *pulseaudio.Client, name string) bool {
 	return false
 }
 
+func SetMidiLed(wr *writer.Writer, control int, state bool) {
+	if state {
+		must(writer.CcOn(wr, uint8(control)))
+	} else {
+		must(writer.CcOff(wr, uint8(control)))
+	}
+}
+
 func main() {
+	_ = decouple.Load()
+	deviceName, exists := decouple.GetString("MUTEWATCHER_MIDI_DEVICE", "")
+	if !exists {
+		panic("you must provide a device name in MUTEWATCHER_MIDI_DEVICE")
+	}
+	deviceChannel, _ := decouple.GetInt("MUTEWATCHER_MIDI_CHANNEL", 0)
+	sinkLedControl, _ := decouple.GetInt("MUTEWATCHER_SINK_LED", 0x30)
+	sourceLedControl, _ := decouple.GetInt("MUTEWATCHER_SOURCE_LED", 0x31)
+
+	drv, err := driver.New()
+	must(err)
+
+	port, err := midi.OpenOut(drv, -1, deviceName)
+	must(err)
+
+	log := log.With().
+		Str("device", port.String()).
+		Int("channel", deviceChannel).Logger()
+
+	wr := writer.New(port)
+	wr.SetChannel(uint8(deviceChannel))
+
 	client, err := pulseaudio.NewClient()
 	must(err)
 
@@ -44,8 +77,17 @@ func main() {
 	info, err := client.ServerInfo()
 	must(err)
 
-	sink_muted := IsSinkMuted(client, info.DefaultSink)
-	source_muted := IsSourceMuted(client, info.DefaultSource)
+	sink_name := info.DefaultSink
+	source_name := info.DefaultSource
+
+	sink_muted := IsSinkMuted(client, sink_name)
+	source_muted := IsSourceMuted(client, source_name)
+
+	log.Info().Str("sink", sink_name).Bool("muted", sink_muted).Msgf("initial sink status")
+	log.Info().Str("source", source_name).Bool("muted", source_muted).Msgf("initial source status")
+
+	SetMidiLed(wr, sinkLedControl, sink_muted)
+	SetMidiLed(wr, sourceLedControl, source_muted)
 
 	for range updates {
 		info, err := client.ServerInfo()
@@ -53,20 +95,14 @@ func main() {
 
 		if sink_muted != IsSinkMuted(client, info.DefaultSink) {
 			sink_muted = !sink_muted
-			if sink_muted {
-				fmt.Printf("sink %s is muted\n", info.DefaultSink)
-			} else {
-				fmt.Printf("sink %s is not muted\n", info.DefaultSink)
-			}
+			SetMidiLed(wr, sinkLedControl, sink_muted)
+			log.Info().Str("sink", sink_name).Bool("muted", sink_muted).Msgf("sink status changed")
 		}
 
 		if source_muted != IsSourceMuted(client, info.DefaultSource) {
 			source_muted = !source_muted
-			if source_muted {
-				fmt.Printf("source %s is muted\n", info.DefaultSource)
-			} else {
-				fmt.Printf("source %s is not muted\n", info.DefaultSource)
-			}
+			SetMidiLed(wr, sourceLedControl, source_muted)
+			log.Info().Str("source", source_name).Bool("muted", source_muted).Msgf("source status changed")
 		}
 	}
 }
